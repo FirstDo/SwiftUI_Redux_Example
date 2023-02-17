4362b2de4ee9f42d36756ed9c94c308294b3935e
dudu <whqtkf12@naver.com>
2023-02-17 17:57:15 +0900

    Initial Commit

---
+++ SwiftUI_Redux_Example/SwiftUI_Redux_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Redux_ExampleApp.swift
+//  SwiftUI_Redux_Example
+//
+//  Created by dudu on 2023/02/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Redux_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
